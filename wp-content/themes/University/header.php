012fcbfc9526955e99bdb9694e278ef57f0533dd
a@fandb.pro <a@fandb.pro>
2019-08-13 08:20:47 +0300

    Two ways of menu creation for WP

--- wp-content/themes/University/header.php
+++ wp-content/themes/University/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
-<body  <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
         <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
@@ -15,8 +15,13 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
+                <?php /* todo Left it here as an example of WP menu
+                wp_nav_menu(array(
+                    'theme_location' => 'headerMenuLocation',
+                ));
+                */ ?>
                 <ul class="min-list group">
-                    <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 5) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Campuses</a></li>
