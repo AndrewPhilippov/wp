c0476baa349815aecb698761257f1fd9d795e481
a@fandb.pro <a@fandb.pro>
2019-08-18 21:17:46 +0300

    Added custom query and handled specific output and pagination

--- wp-content/themes/University/front-page.php
+++ wp-content/themes/University/front-page.php
@@ -18,34 +18,47 @@
                 <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
                 <?php
+                $today = date('Ymd');
                 $homepageEvents = new WP_Query(array(
-                    'posts_per_page' => 2,
                     'post_type' => 'event',
+                    'posts_per_page' => 2,
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value_num', // WHY 'orderby' => 'meta_value_num',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(  // specific to the event_date query
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric' // SURELY optional in this case
+                        )
+                    )
                 ));
+
                 while ($homepageEvents->have_posts()) {
                     $homepageEvents->the_post(); ?>
                     <div class="event-summary">
                         <a class="event-summary__date t-center" href="#">
-                            <span class="event-summary__month"><?php
-                                $eventDate = new DateTime(get_field('event_date'));
-                                echo $eventDate->format('M') ?></span>
-                            <span class="event-summary__day"><?= $eventDate->format('d') ?></span>
+                <span class="event-summary__month"><?php
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M')
+                    ?></span>
+                            <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
                         </a>
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a
-                                        href="<? the_permalink(); ?>"><?= the_title(); ?></a></h5>
-                            <p><? if (has_excerpt()) {
+                                        href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <p><?php if (has_excerpt()) {
                                     echo get_the_excerpt();
                                 } else {
                                     echo wp_trim_words(get_the_content(), 18);
-                                } ?> <a href="<? the_permalink(); ?>"
-                                        class="nu gray">Learn more</a></p>
+                                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
                         </div>
                     </div>
                 <?php }
                 ?>
 
-                <p class="t-center no-margin"><a href="<?= get_post_type_archive_link('event'); ?>"
+                <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>"
                                                  class="btn btn--blue">View All Events</a></p>
 
             </div>
@@ -69,12 +82,11 @@
                         <div class="event-summary__content">
                             <h5 class="event-summary__title headline headline--tiny"><a
                                         href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                            <p><? if (has_excerpt()) {
+                            <p><?php if (has_excerpt()) {
                                     echo get_the_excerpt();
                                 } else {
                                     echo wp_trim_words(get_the_content(), 18);
-                                } ?> <a href="<?php the_permalink(); ?>"
-                                        class="nu gray">Read more</a></p>
+                                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                         </div>
                     </div>
                 <?php }
