c7c351c8f8075f0404938a7da27f4c13a99d86b7
a@fandb.pro <a@fandb.pro>
2019-08-30 10:28:50 +0200

    Complete work on professors section. Added image crops

--- wp-config.php
+++ wp-config.php
@@ -94,3 +94,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+/**
+ * Increase upload file size
+ * */
+@ini_set( 'upload_max_size' , '12M' );
+@ini_set( 'post_max_size', '13M');
+@ini_set( 'memory_limit', '15M' );
