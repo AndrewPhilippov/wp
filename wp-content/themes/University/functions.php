23bc22d4c6df5f41a6ffecec9b2d884d4de7a29b
a@fandb.pro <a@fandb.pro>
2019-10-01 08:06:32 +0300

    Done with work on user roles and login page styling

--- wp-content/themes/University/functions.php
+++ wp-content/themes/University/functions.php
@@ -1,15 +1,15 @@
 <?php
 
-require get_theme_file_path('/inc/search-route.php');
+require get_theme_file_path( '/inc/search-route.php' );
 function university_custom_rest() {
-    register_rest_field('post', 'authorName', array(
-            'get_callback' => function() {
-                return get_the_author();
-            }
-    ));
+	register_rest_field( 'post', 'authorName', array(
+		'get_callback' => function () {
+			return get_the_author();
+		}
+	) );
 }
 
-add_action('rest_api_init', 'university_custom_rest');
+add_action( 'rest_api_init', 'university_custom_rest' );
 
 function pageBanner( $args = null ) {
 
@@ -47,9 +47,9 @@ function university_files() {
 	wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 	wp_enqueue_style( 'university_main_styles', get_stylesheet_uri() );
-	wp_localize_script('main-university-js', 'universityData', array(
-	        'root_url' => get_site_url(),
-    ));
+	wp_localize_script( 'main-university-js', 'universityData', array(
+		'root_url' => get_site_url(),
+	) );
 }
 
 add_action( 'wp_enqueue_scripts', 'university_files' );
@@ -64,30 +64,30 @@ function university_features() {
 
 add_action( 'after_setup_theme', 'university_features' );
 
-function university_adjust_queries($query) {
-	if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
-		$query->set('posts_per_page', -1);
+function university_adjust_queries( $query ) {
+	if ( ! is_admin() AND is_post_type_archive( 'campus' ) AND $query->is_main_query() ) {
+		$query->set( 'posts_per_page', - 1 );
 	}
 
-	if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
-		$query->set('orderby', 'title');
-		$query->set('order', 'ASC');
-		$query->set('posts_per_page', -1);
+	if ( ! is_admin() AND is_post_type_archive( 'program' ) AND $query->is_main_query() ) {
+		$query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
+		$query->set( 'posts_per_page', - 1 );
 	}
 
-	if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
-		$today = date('Ymd');
-		$query->set('meta_key', 'event_date');
-		$query->set('orderby', 'meta_value_num');
-		$query->set('order', 'ASC');
-		$query->set('meta_query', array(
+	if ( ! is_admin() AND is_post_type_archive( 'event' ) AND $query->is_main_query() ) {
+		$today = date( 'Ymd' );
+		$query->set( 'meta_key', 'event_date' );
+		$query->set( 'orderby', 'meta_value_num' );
+		$query->set( 'order', 'ASC' );
+		$query->set( 'meta_query', array(
 			array(
-				'key' => 'event_date',
+				'key'     => 'event_date',
 				'compare' => '>=',
-				'value' => $today,
-				'type' => 'numeric'
+				'value'   => $today,
+				'type'    => 'numeric'
 			)
-		));
+		) );
 	}
 }
 
@@ -102,3 +102,43 @@ function universityMapKey( $api ) {
 add_filter( 'acf/fields/google_map/api', 'universityMapKey' );
 
 // Redirect subscriber accounts out of admin and onto homepage
+add_action( 'admin_init', 'redirectSubsToFrontend' );
+
+function redirectSubsToFrontend() {
+	$ourCurrentUser = wp_get_current_user();
+
+	if ( count( $ourCurrentUser->roles ) == 1 AND $ourCurrentUser->roles[0] == 'subscriber' ) {
+		wp_redirect( site_url( '/' ) );
+		exit;
+	}
+}
+
+add_action( 'wp_loaded', 'noSubsAdminBar' );
+
+function noSubsAdminBar() {
+	$ourCurrentUser = wp_get_current_user();
+
+	if ( count( $ourCurrentUser->roles ) == 1 AND $ourCurrentUser->roles[0] == 'subscriber' ) {
+		show_admin_bar( false );
+	}
+}
+
+// Customize Login Screen
+add_filter( 'login_headerurl', 'ourHeaderUrl' );
+
+function ourHeaderUrl() {
+	return esc_url( site_url( '/' ) );
+}
+
+add_action( 'login_enqueue_scripts', 'ourLoginCSS' );
+
+function ourLoginCSS() {
+	wp_enqueue_style( 'university_main_styles', get_stylesheet_uri() );
+	wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
+}
+
+add_filter( 'login_headertitle', 'ourLoginTitle' );
+
+function ourLoginTitle() {
+	return get_bloginfo( 'name' );
+}
--- wp-content/themes/University/header.php
+++ wp-content/themes/University/header.php
@@ -10,8 +10,9 @@
     <div class="container">
         <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
         </h1>
-        <a href="<?= esc_url(site_url('/')) ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
-                                                                       aria-hidden="true"></i></a>
+        <a href="<?= esc_url( site_url( '/' ) ) ?>" class="js-search-trigger site-header__search-trigger"><i
+                    class="fa fa-search"
+                    aria-hidden="true"></i></a>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
@@ -33,16 +34,19 @@
                 </ul>
             </nav>
             <div class="site-header__util">
-                <?php if(is_user_logged_in()) { ?>
-                    <a href="<?= wp_logout_url() ?>" class="btn btn--small  btn--dark-orange float-left btn--with-photo">
-                        <span class="site-header__avatar"><?= get_avatar(get_current_user_id(), 60) ?></span>
+				<?php if ( is_user_logged_in() ) { ?>
+                    <a href="<?= wp_logout_url() ?>"
+                       class="btn btn--small  btn--dark-orange float-left btn--with-photo">
+                        <span class="site-header__avatar"><?= get_avatar( get_current_user_id(), 60 ) ?></span>
                         <span class="btn__text">Logout</span>
                     </a>
-                <?php } else { ?>
-                    <a href="<?= esc_url(site_url('/wp-login.php')) ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
-                    <a href="<?= esc_url(site_url('/wp-signup.php')) ?>" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
-                <?php } ?>
-                <a href="<?= esc_url(site_url('/')) ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
+				<?php } else { ?>
+                    <a href="<?= wp_login_url() ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="<?= wp_registration_url() ?>" class="btn btn--small  btn--dark-orange float-left">Sign
+                        Up</a>
+				<?php } ?>
+                <a href="<?= esc_url( site_url( '/' ) ) ?>" class="search-trigger js-search-trigger"><i
+                            class="fa fa-search" aria-hidden="true"></i></a>
             </div>
         </div>
     </div>
