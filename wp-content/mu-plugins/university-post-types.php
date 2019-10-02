bee07a3e56b47e3153b608de29659b620eceff8b
a@fandb.pro <a@fandb.pro>
2019-10-02 08:22:45 +0300

    Started to work on crud. Got a problem to see my 'notes' posts on front

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -39,33 +39,50 @@ function university_post_types() {
 
 //    Program Post Type
 	register_post_type( 'program', array(
-		'supports'    => array( 'title' ),
-		'rewrite'     => array( 'slug' => 'programs' ),
-		'has_archive' => true,
-		'public'      => true,
-		'labels'      => array(
+		'supports'     => array( 'title' ),
+		'rewrite'      => array( 'slug' => 'programs' ),
+		'has_archive'  => true,
+		'public'       => true,
+		'labels'       => array(
 			'name'          => 'Programs',
 			'add_new_item'  => 'Add New Program',
 			'edit_item'     => 'Edit Program',
 			'all_items'     => 'All Programs',
 			'singular_name' => 'Program',
 		),
-		'menu_icon'   => 'dashicons-awards',
+		'menu_icon'    => 'dashicons-awards',
 	) );
 
 //    Professor Post Type
 	register_post_type( 'professor', array(
-		'supports'    => array( 'title', 'editor', 'thumbnail' ),
-		'has_archive' => true,
-		'public'      => true,
-		'labels'      => array(
+		'show_in_rest' => true,
+		'supports'     => array( 'title', 'editor', 'thumbnail' ),
+		'has_archive'  => true,
+		'public'       => true,
+		'labels'       => array(
 			'name'          => 'Professors',
 			'add_new_item'  => 'Add New Professor',
 			'edit_item'     => 'Edit Professor',
 			'all_items'     => 'All Professors',
 			'singular_name' => 'Professor',
 		),
-		'menu_icon'   => 'dashicons-welcome-learn-more',
+		'menu_icon'    => 'dashicons-welcome-learn-more',
+	) );
+
+//  Notes Post Type
+	register_post_type( 'note', array(
+		'show_in_rest' => true,
+		'supports'     => array( 'title', 'editor' ),
+		'public'       => false,
+		'show_ui'      => true,
+		'labels'       => array(
+			'name'          => 'Notes',
+			'add_new_item'  => 'Add New Note',
+			'edit_item'     => 'Edit Note',
+			'all_items'     => 'All Notes',
+			'singular_name' => 'Note'
+		),
+		'menu_icon'    => 'dashicons-welcome-write-blog'
 	) );
 }
 
