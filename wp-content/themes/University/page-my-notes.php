38af50d97d8e347dbf701dfd7b9143ea59d94128
a@fandb.pro <a@fandb.pro>
2019-10-05 15:45:14 +0300

    Working on edit-note functionality

--- wp-content/themes/University/page-my-notes.php
+++ wp-content/themes/University/page-my-notes.php
@@ -19,16 +19,16 @@ while ( have_posts() ) {
 			$userNotes = new WP_Query( array(
 				'post_type'      => 'note',
 				'posts_per_page' => -1,
-				'author'         => get_current_user_id()
+//				'author'         => get_current_user_id()
 			) );
 
 			while ( $userNotes->have_posts() ) {
 				$userNotes->the_post(); ?>
                 <li data-id="<?php the_ID(); ?>">
-                    <input readonly class="note-title-field" value="<?php echo esc_attr( get_the_title() ); ?>">
+                    <input readonly class="note-title-field" value="<?= esc_attr( get_the_title() ) ?>">
                     <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
                     <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
-                    <textarea readonly class="note-body-field"><?php echo esc_attr( get_the_content() ); ?></textarea>
+                    <textarea readonly class="note-body-field"><?= esc_attr( get_the_content() ) ?></textarea>
                     <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
                 </li>
 			<?php }
